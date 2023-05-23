034509f48434b4a46671449997c95c523e8b8e36
Micael Teixeira <mikatx2@hotmail.com>
2023-05-23 22:00:41 +0100

    Fix render scripts and sharders, project working again

--- Assets/Scripts/Shader/BloomEffectShader.cs
+++ Assets/Scripts/Shader/BloomEffectShader.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
 
-[VolumeComponentMenuForRenderPipeLine("Custom/Comic Bloom", typeof(UniversalRenderPipeLine))]
+[VolumeComponentMenuForRenderPipeline("Custom/Comic Bloom", typeof(UniversalRenderPipeline))]
 public class BloomEffectShader : VolumeComponent, IPostProcessComponent
 {
     [Header("Bloom Settings")]
@@ -13,7 +13,7 @@ public class BloomEffectShader : VolumeComponent, IPostProcessComponent
 
     public FloatParameter intensity = new FloatParameter(1, true);
 
-    public ClampedFloatParameter scatter = new ClampedFloatParameter(0.7, 0, 1, true);
+    public ClampedFloatParameter scatter = new ClampedFloatParameter(0.7f, 0, 1, true);
 
     public IntParameter clamp = new IntParameter(65472, true);
 
